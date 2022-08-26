091fadef1c51c87e763fedffc2009205bcc44b1f
Juan Palomino <juan.palomino@bairesdev.com>
2022-08-26 00:31:55 -0500

    format

--- src/unir_devops_medex/settings.py
+++ src/unir_devops_medex/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +75,11 @@ WSGI_APPLICATION = "unir_devops_medex.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }}
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 else:
     DATABASES = {
         "default": {
